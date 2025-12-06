dbcc5ec71292b5060f666f79689af89fae332fe3
LE PHUC THUAN <119940327+ThuanPhuc27@users.noreply.github.com>
2025-12-06 13:27:48 +0700

    Update aws-cli-install.sh

--- Cloud/AWS/aws-cli-install.sh
+++ Cloud/AWS/aws-cli-install.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
-curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+
+aws --version
